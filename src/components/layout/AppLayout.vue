b1da2d8758085d5c4302086eac68f4640a654026
jipenalver <jianchris2k14@gmail.com>
2025-01-17 17:49:26 +0800

    style: added base layout

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive>
+    <v-app :theme="theme">
+      <v-app-bar class="px-3">
+        <v-app-bar-title> <h3>Vue Todo</h3> </v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text="Toggle Theme"
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <slot name="content"></slot>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppLayout from '@/components/layout/AppLayout.vue'
+</script>
 
 <template>
-  <main></main>
+  <AppLayout>
+    <template #content>
+      <v-container fluid>
+        <v-row class="d-flex justify-center" dense>
+          <v-col cols="12" md="8">
+            <h1 class="text-center mt-5">Vue Todo App</h1>
+
+            <v-divider thickness="2" class="my-5"></v-divider>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+  </AppLayout>
 </template>
