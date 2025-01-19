c18eec3ced8f8e661747a36226b3b93739794d8b
jipenalver <jianchris2k14@gmail.com>
2025-01-19 12:20:09 +0800

    refactor: update helpers

--- src/utils/helpers/others.ts
+++ src/utils/helpers/others.ts
@@ -18,20 +18,35 @@ export const getSlugText = (name: string) => {
 }
 
 // 👉 Money Format Text
-export const getMoneyText = (value: number) => {
-  if (isNaN(value)) return '₱0.00'
+export const getMoneyText = (value: string | number) => {
+  if (isNaN(Number(value))) return '₱0.00'
 
   return new Intl.NumberFormat('en-PH', {
     style: 'currency',
     currency: 'PHP',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  }).format(value)
+  }).format(Number(value))
+}
+
+// 👉 Pad String Left
+export const getPadLeftText = (value: string | number, length = 4, char = '0') => {
+  value = String(value)
+  if (value.length >= length) return value
+  return char.repeat(length - value.length) + value
 }
 
 // 👉 Precise Number
-export const getPreciseNumber = (value: number): number => {
+export const getPreciseNumber = (value: number) => {
   return Math.round(value * 100) / 100
+}
+
+// 👉 Accumulated Number
+export const getAccumulatedNumber = <T extends Record<string, unknown>>(
+  object: T[],
+  key: string,
+) => {
+  return object.reduce((acc, cur) => acc + (isNaN(Number(cur[key])) ? 0 : Number(cur[key])), 0)
 }
 
 // 👉 Alpha-numeric Random Code
@@ -43,19 +58,12 @@ export const getRandomCode = (length = 6) => {
   )
 }
 
-// 👉 Pad String Left
-export const getPadLeftText = (value: string | number, length = 4, char = '0') => {
-  value = String(value)
-  if (value.length >= length) return value
-  return char.repeat(length - value.length) + value
-}
-
 // 👉 File Extraction of Object, for 1 File/Image
-export const fileExtract = (event: Event): Promise<{ fileObject: File; fileUrl: string }> => {
+export const fileExtract = (event: Event) => {
   return new Promise((resolve, reject) => {
     const { files } = event.target as HTMLInputElement
 
-    if (!files || !files.length || files.length === 0) return reject(new Error('No files selected'))
+    if (!files || !files.length) return reject(new Error('No files selected'))
 
     const fileReader = new FileReader()
     fileReader.readAsDataURL(files[0])
@@ -71,40 +79,68 @@ export const fileExtract = (event: Event): Promise<{ fileObject: File; fileUrl: 
 }
 
 // 👉 File Extraction of Object, for Multiple Files/Images
-export const filesExtract = (event: Event): Promise<{ fileObject: File[]; fileUrl: string[] }> => {
+export const filesExtract = (event: Event) => {
   return new Promise((resolve, reject) => {
     const { files } = event.target as HTMLInputElement
 
-    if (!files || !files || files.length === 0) {
-      return reject(new Error('No files selected'))
-    }
+    if (!files || !files.length) return reject(new Error('No files selected'))
 
-    const fileObject = Array.from(files)
-    const fileReaders = fileObject.map(
-      (file) =>
-        new Promise<{ fileObject: File; fileUrl: string }>((resolve, reject) => {
-          const fileReader = new FileReader()
-          fileReader.readAsDataURL(file)
+    const fileObjects = Array.from(files)
+    const fileReaders = fileObjects.map((file) => {
+      return new Promise<{ fileObject: File; fileUrl: string }>((resolve, reject) => {
+        const fileReader = new FileReader()
+        fileReader.readAsDataURL(file)
 
-          fileReader.onload = () => {
-            if (typeof fileReader.result === 'string') {
-              resolve({ fileObject: file, fileUrl: fileReader.result })
-            } else {
-              reject(new Error('Failed to read file as Data URL'))
-            }
-          }
+        fileReader.onload = () => {
+          if (typeof fileReader.result === 'string')
+            resolve({ fileObject: file, fileUrl: fileReader.result })
+          else reject(new Error('Failed to read file as Data URL'))
+        }
 
-          fileReader.onerror = () => reject(new Error('Error reading file'))
-        }),
-    )
+        fileReader.onerror = () => reject(new Error('Error reading file'))
+      })
+    })
 
     Promise.all(fileReaders)
       .then((results) => {
-        const fileUrl = results.map((result) => result.fileUrl)
-        resolve({ fileObject, fileUrl })
+        const fileUrls = results.map((result) => result.fileUrl)
+        resolve({ fileObjects, fileUrls })
       })
       .catch((error) => reject(error))
   })
+}
+
+// 👉 Fix v-date-input; prepare local dates in form
+type FormData = {
+  [key: string]: string | number | Date | undefined
+}
+export const prepareFormDates = (formData: FormData, dateColumns = []) => {
+  dateColumns.forEach((dateColumn) => {
+    if (formData[dateColumn]) {
+      const dateValue = new Date(formData[dateColumn])
+      formData[dateColumn] = dateValue.toLocaleDateString('en-US', { timeZone: 'Asia/Manila' })
+    }
+  })
+
+  return formData
+}
+
+// 👉 Fix v-date-input; prepare local date in form
+export const prepareDate = (date: Date | string) => {
+  const dateValue = new Date(date)
+  return dateValue.toLocaleDateString('en-US', { timeZone: 'Asia/Manila' })
+}
+
+// 👉 Get date in ISO format without UTC conversion
+export const getISODate = (date: Date | string) => {
+  const dateValue = new Date(date)
+
+  // Extract components in the local timezone
+  const year = dateValue.getFullYear()
+  const month = String(dateValue.getMonth() + 1).padStart(2, '0') // Months are 0-based
+  const day = String(dateValue.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
 }
 
 // 👉 Generate CSV
@@ -131,11 +167,4 @@ export const generateCSVTrim = (string: string) => {
   if (typeof string !== 'string' || !string.trim()) return ''
 
   return string.replace(/,/g, ' ').replace(/\s+/g, ' ').trim()
-}
-
-// 👉 Parse Object Trimming
-export const objectValueTrim = (string: string) => {
-  if (typeof string !== 'string' || !string.trim()) return ''
-
-  return string.replace(/\s+/g, ' ').trim()
 }
